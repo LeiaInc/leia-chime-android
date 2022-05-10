ad8491031456a757d2bf69f39aea82cfaad6389f
Lin <linsang@amazon.com>
2022-05-10 13:40:29 -0700

    Add global lock for egl related operations

---
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/internal/video/gl/ShareEglLock.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazonaws.services.chime.sdk.meetings.internal.video.gl
+
+class ShareEglLock {
+    companion object {
+        val Lock = object {}
+    }
+}
